5afcfa8347ed2586069899b931f7e659551cf22a
Sean Murphy <sean@iamseanmurphy.com>
2025-05-27 11:18:27 +0000

    12.7.5

--- review-widgets-for-booking-com/uninstall.php
+++ review-widgets-for-booking-com/uninstall.php
@@ -3,6 +3,6 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
 die;
 }
 require_once plugin_dir_path( __FILE__ ) . 'trustindex-plugin.class.php';
-$trustindex_pm_booking = new TrustindexPlugin_booking("booking", __FILE__, "12.7.4", "Widgets for Booking.com Reviews", "Booking.com");
+$trustindex_pm_booking = new TrustindexPlugin_booking("booking", __FILE__, "12.7.5", "Widgets for Booking.com Reviews", "Booking.com");
 $trustindex_pm_booking->uninstall();
 ?>
